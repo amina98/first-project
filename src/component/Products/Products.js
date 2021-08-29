4348cc4a5bad627335aa562cd7aca311f0881f0d
Mahdi Jafarzadeh <aminjafar1375@gmail.com>
2021-08-29 17:25:51 +0430

    updating products and add slider

--- src/component/Products/Products.js
+++ src/component/Products/Products.js
@@ -4,9 +4,18 @@ import Product from './Product';
 
 import styles from './Products.module.css';
 
-import Image1 from '../../image/products/0002.jpg';
-import Image2 from '../../image/products/0003.jpg';
-import Image3 from '../../image/products/0004.jpg';
+import Image1 from '../../image/products/0001.jpg';
+import Image2 from '../../image/products/0002.jpg';
+import Image3 from '../../image/products/0003.jpg';
+import Image4 from '../../image/products/0004.jpg';
+import Image5 from '../../image/products/0005.jpg';
+import Image6 from '../../image/products/0006.jpg';
+import Image7 from '../../image/products/0007.jpg';
+import Image8 from '../../image/products/0008.jpg';
+import Image9 from '../../image/products/0009.jpg';
+import Image10 from '../../image/products/0010.jpg';
+import Image11 from '../../image/products/0011.jpg';
+import Image12 from '../../image/products/0012.jpg';
 
 class Products extends Component {
     constructor(){
@@ -14,17 +23,17 @@ class Products extends Component {
         this.state = {
             data:[
             {id:1 ,image:Image1, name:"محصول شماره 1", cost:"10.000"},
-            {id:2 ,image:Image2, name:"محصول شماره 1", cost:"15.000"},
-            {id:3 ,image:Image3, name:"محصول شماره 1", cost:"12.000"},
-            {id:4 ,image:Image1, name:"محصول شماره 1", cost:"10.000"},
-            {id:5 ,image:Image2, name:"محصول شماره 1", cost:"15.000"},
-            {id:6 ,image:Image3, name:"محصول شماره 1", cost:"12.000"},
-            {id:7 ,image:Image1, name:"محصول شماره 1", cost:"10.000"},
-            {id:8 ,image:Image2, name:"محصول شماره 1", cost:"15.000"},
-            {id:9 ,image:Image3, name:"محصول شماره 1", cost:"12.000"},
-            {id:10 ,image:Image1, name:"محصول شماره 1", cost:"10.000"},
-            {id:11 ,image:Image2, name:"محصول شماره 1", cost:"15.000"},
-            {id:12 ,image:Image3, name:"محصول شماره 1", cost:"12.000"}
+            {id:2 ,image:Image2, name:"محصول شماره 2", cost:"15.000"},
+            {id:3 ,image:Image3, name:"محصول شماره 3", cost:"12.000"},
+            {id:4 ,image:Image4, name:"محصول شماره 4", cost:"10.000"},
+            {id:5 ,image:Image5, name:"محصول شماره 5", cost:"15.000"},
+            {id:6 ,image:Image6, name:"محصول شماره 6", cost:"12.000"},
+            {id:7 ,image:Image7, name:"محصول شماره 7", cost:"10.000"},
+            {id:8 ,image:Image8, name:"محصول شماره 8", cost:"15.000"},
+            {id:9 ,image:Image9, name:"محصول شماره 9", cost:"12.000"},
+            {id:10 ,image:Image10, name:"محصول شماره 10", cost:"10.000"},
+            {id:11 ,image:Image11, name:"محصول شماره 11", cost:"15.000"},
+            {id:12 ,image:Image12, name:"محصول شماره 12", cost:"12.000"}
             ]
     }
     }
--- src/component/slider/Slider.js
+++ src/component/slider/Slider.js
@@ -1,25 +1,80 @@
 import React, { Component } from "react";
 
-import styles from './Slider.module.css';
+import styles from "./Slider.module.css";
 
-import Landing from '../../image/landing.png';
+import Landing from "../../image/landing.png";
+import Img1 from "../../image/img1.jpg";
+import Img2 from "../../image/img2.jpg";
+import Img3 from "../../image/img3.jpg";
+import Img4 from "../../image/img4.jpg";
 
 class Slider extends Component {
+    constructor() {
+        super();
+        this.slideIndex = 1;
+        this.mySlides = React.createRef();
+    }
+    nextSlides = () => {
+        this.showSlides((this.slideIndex += 1));
+    };
+    prevSlides = () => {
+        this.showSlides((this.slideIndex += -1));
+    };
+    showSlides = (n) => {
+        console.log(this.slideIndex);
+        if (n > this.mySlides.current.childNodes.length) {
+            this.slideIndex = 1;
+        }
+        if (n < 1) {
+            this.slideIndex= this.mySlides.current.childNodes.length;
+        }
+        console.log(this.slideIndex);
+        for (let i = 0; i < this.mySlides.current.childNodes.length; i++) {
+            this.mySlides.current.childNodes[i].style.display = "none";
+        }
+        this.mySlides.current.childNodes[this.slideIndex - 1].style.display = "block";
+        
+    };
+    clickHandler = () => {
+        console.log(this.mySlides.current.childNodes);
+    };
     render() {
         return (
             <div className={styles.landing}>
                 <img src={Landing} alt="landing" />
+                <div ref={this.mySlides} className={styles.slideshow}>
+                    <div className={styles.mySlides}  style={{display :"block"}}>
+                        <img src={Img1} alt="slider" />
+                    </div>
+                    <div className={styles.mySlides}>
+                        <img src={Img2} alt="slider" />
+                    </div>
+                    <div className={styles.mySlides}>
+                        <img src={Img3} alt="slider" />
+                    </div>
+                    <div className={styles.mySlides}>
+                        <img src={Img4} alt="slider" />
+                    </div>
+                </div>
+                {/* <button onClick={this.nextSlides}>Next</button>
+                <button onClick={this.prevSlides}>prev</button> */}
+                <a className={styles.prev} onClick={this.nextSlides}>
+                    &#10094;
+                </a>
+                <a className={styles.next} onClick={this.prevSlides}>
+                    &#10095;
+                </a>
                 <div className={styles.landingContent}>
-              <h1>در محصولات سایت جستجو کنید ...</h1>
-              <br />
-              <input
-                type="text"
-                name=""
-                id=""
-                placeholder="نام محصول خود را وارد کنید ..."
-              />
-              <a href="#">جستجو کنید</a>
-            </div>
+                    <h1>در محصولات سایت جستجو کنید ...</h1>
+                    <br />
+                    <input
+                        type="text"
+                        name=""
+                        id=""
+                        placeholder="نام محصول خود را وارد کنید ..."
+                    />
+                    <a href="#">جستجو کنید</a>
+                </div>
             </div>
         );
     }
