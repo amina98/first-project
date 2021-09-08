98a1882b4e8a93d65f50b1d3848fb5fe56014ed9
Mahdi Jafarzadeh <aminjafar1375@gmail.com>
2021-09-08 17:23:47 +0430

    improve slider

--- src/component/slider/Slider.js
+++ src/component/slider/Slider.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useRef } from "react";
 
 import styles from "./Slider.module.css";
 
@@ -6,73 +6,81 @@ import Img1 from "../../image/img1.jpg";
 import Img2 from "../../image/img2.jpg";
 import Img3 from "../../image/img3.jpg";
 import Img4 from "../../image/img4.jpg";
+let slideIndex = 1;
 
-class Slider extends Component {
-    constructor() {
-        super();
-        this.slideIndex = 1;
-        this.mySlides = React.createRef();
-    }
-    nextSlides = () => {
-        this.showSlides((this.slideIndex += 1));
-    };
-    prevSlides = () => {
-        this.showSlides((this.slideIndex += -1));
-    };
-    showSlides = (n) => {
-        if (n > this.mySlides.current.childNodes.length) {
-            this.slideIndex = 1;
-        }
-        if (n < 1) {
-            this.slideIndex= this.mySlides.current.childNodes.length;
-        }
-        for (let i = 0; i < this.mySlides.current.childNodes.length; i++) {
-            this.mySlides.current.childNodes[i].style.display = "none";
-        }
-        this.mySlides.current.childNodes[this.slideIndex - 1].style.display = "block";
-        
-    };
-    searchHandler = event=>{
+const Slider = () => {
+    const image = useRef(null);
+    const images = [Img1, Img2, Img3, Img4];
+
+    const searchHandler = (event) => {
         event.preventDefault();
-    }
-    render() {
-        return (
-            <div className={styles.landing}>
-                <div ref={this.mySlides} className={styles.slideshow}>
-                    <div className={`${styles.mySlides} ${styles.fade}`}  style={{display :"block"}}>
-                        <img src={Img1} alt="slider" />
-                    </div>
-                    <div className={`${styles.mySlides} ${styles.fade}`}>
-                        <img src={Img2} alt="slider" />
-                    </div>
-                    <div className={`${styles.mySlides} ${styles.fade}`}>
-                        <img src={Img3} alt="slider" />
-                    </div>
-                    <div className={`${styles.mySlides} ${styles.fade}`}>
-                        <img src={Img4} alt="slider" />
-                    </div>
-                </div>
-                
-                <i className={`${styles.prev} fas fa-chevron-right`} onClick={this.nextSlides}>
-                    
-                </i>
-                <i className={`${styles.next} fas fa-chevron-left`} onClick={this.prevSlides}>
-                    
-                </i>
-                <div className={styles.landingContent}>
-                    <h1>در محصولات سایت جستجو کنید ...</h1>
-                    <br />
-                    <input
-                        type="text"
-                        name=""
-                        id=""
-                        placeholder="نام محصول خود را وارد کنید ..."
-                    />
-                    <button type="submit" onClick={this.searchHandler}>جستجو کنید</button>
-                </div>
+    };
+    const nextSlides = () => {
+        if (slideIndex >= images.length) {
+            slideIndex = 1;
+        } else if (slideIndex <= 0) {
+            slideIndex = images.length;
+        } else {
+            slideIndex += 1;
+        }
+        console.log(images[slideIndex - 1]);
+        image.current.style.backgroundImage = `url(${images[slideIndex - 1]})`;
+    };
+    const prevSlides = () => {
+        if (slideIndex > images.length) {
+            slideIndex = 1;
+        } else if (slideIndex <= 1) {
+            slideIndex = images.length;
+        } else {
+            slideIndex -= 1;
+        }
+        image.current.style.backgroundImage = `url(${images[slideIndex - 1]})`;
+    };
+    useEffect(() => {
+        setInterval(() => {
+            if (slideIndex > images.length) {
+                slideIndex = 1;
+            } else if (slideIndex <= 1) {
+                slideIndex = images.length;
+            } else {
+                slideIndex -= 1;
+            }
+            image.current.style.backgroundImage = `url(${
+                images[slideIndex - 1]
+            })`;
+        }, 5000);
+    }, []);
+
+    return (
+        <div className={styles.landing}>
+            <div
+                ref={image}
+                className={`${styles.slideshow} ${styles.fade}`}
+            ></div>
+
+            <i
+                className={`${styles.prev} fas fa-chevron-right`}
+                onClick={nextSlides}
+            ></i>
+            <i
+                className={`${styles.next} fas fa-chevron-left`}
+                onClick={prevSlides}
+            ></i>
+            <div className={styles.landingContent}>
+                <h1>در محصولات سایت جستجو کنید ...</h1>
+                <br />
+                <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="نام محصول خود را وارد کنید ..."
+                />
+                <button type="submit" onClick={searchHandler}>
+                    جستجو کنید
+                </button>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default Slider;
